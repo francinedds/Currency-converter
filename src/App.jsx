ba404711ae953cca7b1774bb6e31d025dd5735fe
francine <francined71@gmail.com>
2024-08-28 22:01:46 -0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ConverterForm from "./components/ConverterForm";
 const App = () => {
   return (
     <div className="currency-converter">
-      <h2 className="converter-title">Current Converter</h2>  
+      <h2 className="converter-title">Currency Converter</h2>  
       <ConverterForm />
     </div>
   )
